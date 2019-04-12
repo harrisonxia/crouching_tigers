101372dffefc58961ed84b2e230d20b580e4fc17
harrisonx <engcxia@gmail.com>
2019-04-11 17:41:41 -0700

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/material-dashboard-react/precache-manifest.bdad59f677e9814d9c1decdc439ec27b.js"
+  "/temp-host/precache-manifest.ca9699039b210807ad2a52918c9f5f76.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/material-dashboard-react/index.html", {
+workbox.routing.registerNavigationRoute("/temp-host/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

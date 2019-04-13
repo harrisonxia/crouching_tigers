11db0f7e871cdc26759b92562be3333f8a96040a
harrisonx <engcxia@gmail.com>
2019-04-12 17:01:48 -0700

    update

---
+++ precache-manifest.4f65ebf13ef0b6077d98eea75442a912.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c95ce4eb42c767d8a467",
+    "url": "/crouching_tigers/static/css/main.fdb4f9b1.chunk.css"
+  },
+  {
+    "revision": "c95ce4eb42c767d8a467",
+    "url": "/crouching_tigers/static/js/main.c95ce4eb.chunk.js"
+  },
+  {
+    "revision": "0a7f6d120809ff1bf068",
+    "url": "/crouching_tigers/static/js/runtime~main.0a7f6d12.js"
+  },
+  {
+    "revision": "66c3d60741b3522bdbe0",
+    "url": "/crouching_tigers/static/css/2.a9ec50bc.chunk.css"
+  },
+  {
+    "revision": "66c3d60741b3522bdbe0",
+    "url": "/crouching_tigers/static/js/2.66c3d607.chunk.js"
+  },
+  {
+    "revision": "43ac2c023db09b3fdec9115cbff9f5aa",
+    "url": "/crouching_tigers/static/media/elon.43ac2c02.png"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/crouching_tigers/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "143864f748623c3e31b380753847080f",
+    "url": "/crouching_tigers/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/temp-host/precache-manifest.ca9699039b210807ad2a52918c9f5f76.js"
+  "/crouching_tigers/precache-manifest.4f65ebf13ef0b6077d98eea75442a912.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/temp-host/index.html", {
+workbox.routing.registerNavigationRoute("/crouching_tigers/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
